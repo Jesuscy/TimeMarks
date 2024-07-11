7e7da72e85b12b0ea02fca1f8773622d48a13769
Jesuscy <Jesuscarrascoyel.mur@gmail.com>
2024-07-11 18:43:39 +0200

    Add routes to server Add GitIgnore

--- Backend/server.cjs
+++ Backend/server.cjs
@@ -1,5 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const activityRouter = require('./src/routes/activity.routes')
+const groupRouter = require('./src/routes/group.routes')
+const userRouter = require('./src/routes/user.routes')
 
 const { connect } = require("./src/utils/db");
 
@@ -9,12 +11,17 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+//Routers
+app.use("/activitys",activityRouter);
+app.use("/groups",groupRouter);
+app.use("/users",userRouter);
 
 
 app.get("/", (request, response) => {
@@ -24,6 +31,7 @@ app.get("/", (request, response) => {
   });
 });
 connect();
+
 // Start server
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
